42885fc43bc612cd8af6b19c168537f8577b92b8
hnlq715 <hnlq.sysu@gmail.com>
2017-07-12 18:42:56 +0800

    compatible for go1.7

---
+++ doggy_1.8.go
@@ -0,0 +1,41 @@
+// +build go1.8
+
+package doggy
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/hnlq715/xgen/utils"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+// ListenAndServeGracefully always returns a non-nil error.
+func ListenAndServeGracefully(handler http.Handler) error {
+	l := utils.LogFromContext(context.Background())
+
+	h := &http.Server{Addr: viper.GetString("listen"), Handler: handler}
+	go func() {
+		if err := h.ListenAndServe(); err != nil {
+			l.Error("h.ListenAndServe failed", zap.Error(err))
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+
+	l.Info("Shutting down the server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	h.Shutdown(ctx)
+
+	l.Info("Server gracefully stopped")
+	return nil
+}
